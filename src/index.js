f49a2a2a31eab6d53d00bbfc69272d3d88daaf5f
Vaidant123 <vaidanttomarlk@gmail.com>
2024-05-02 15:43:32 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Modal from "./Modal";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Modal />
+    <App />
   </StrictMode>
 );
